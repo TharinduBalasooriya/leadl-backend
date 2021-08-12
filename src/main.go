e90e30bb47bb7c97cfab3a7a061ed46a458f99f2
Tharindu Balasooriya <53065333+TharinduBalasooriya@users.noreply.github.com>
2021-08-12 16:40:33 +0530

    asdsa

--- src/main.go
+++ src/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	
 	"log"
 	"net/http"
 	"os"
@@ -30,9 +29,9 @@ func main() {
 	//Starting the API server
 	router := routes.LogRoutes()
 
-	os.MkdirAll("temp",0755)
-	os.MkdirAll("localstorage",0755)
-	os.MkdirAll("debug_env",0755)
+	os.MkdirAll("temp", 0755)
+	os.MkdirAll("localstorage", 0755)
+	os.MkdirAll("debug_env", 0755)
 
 	//Load the env file
 	LoadEnv()
@@ -41,8 +40,6 @@ func main() {
 	// })
 	http.Handle("/", router)
 	//log.Println("Server Started localhost :3000")
-	log.Fatal(http.ListenAndServe(":" + os.Getenv("PORT"), handlers.CORS(handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type", "Authorization"}), handlers.AllowedMethods([]string{"GET", "POST", "PUT", "HEAD", "OPTIONS"}), handlers.AllowedOrigins([]string{"*"}))(router)))
-
-
+	log.Fatal(http.ListenAndServe(":"+os.Getenv("PORT"), handlers.CORS(handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type", "Authorization"}), handlers.AllowedMethods([]string{"GET", "POST", "PUT", "HEAD", "OPTIONS"}), handlers.AllowedOrigins([]string{"https://leadl-web.herokuapp.com/"}))(router)))
 
 }
