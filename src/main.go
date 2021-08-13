6251715fb6fa110ec0817dc0e6324e949c5525af
Tharindu Balasooriya <53065333+TharinduBalasooriya@users.noreply.github.com>
2021-08-13 11:55:48 +0530

    Update main.go

--- src/main.go
+++ src/main.go
@@ -40,6 +40,6 @@ func main() {
 	// })
 	http.Handle("/", router)
 	//log.Println("Server Started localhost :3000")
-	log.Fatal(http.ListenAndServe(":"+os.Getenv("PORT"), handlers.CORS(handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type", "Authorization"}), handlers.AllowedMethods([]string{"GET", "POST", "PUT", "HEAD", "OPTIONS"}), handlers.AllowedOrigins([]string{"https://leadl-web.herokuapp.com/","*"}))(router)))
+	log.Fatal(http.ListenAndServe(":"+os.Getenv("PORT"), handlers.CORS(handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type", "Authorization","Token"}), handlers.AllowedMethods([]string{"GET", "POST", "PUT", "HEAD", "OPTIONS"}), handlers.AllowedOrigins([]string{"https://leadl-web.herokuapp.com","*"}))(router)))
 
 }
